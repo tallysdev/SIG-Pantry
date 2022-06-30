9f827dd9abc97cfcc7d0daee5ffaf429399589ea
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-06-30 16:13:18 -0300

    main: add esboço da tela inicial

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 #modulo de estoque de itens.
 #modulo de itens.
 
+import os
+
+os.system('clear')
+print('#########################################')
+print('########## Menu Principal ###############')
+print('#########################################')
+print()
+print('\t introdução .....')
+print('Modolu de Produtos')
+print()
+print('Modulo de Itens')
+print()
+print('Modulo de Controle')
