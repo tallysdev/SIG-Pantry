cd66cfca9fc023c46d2905afc3fe396452614c7d
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-09 18:36:37 -0300

    tallys: add nova forma de cadastro de itens

--- feira.py
+++ feira.py
@@ -8,17 +8,17 @@ itens_p = []
 itens = []
 
 feira = {}
-
+codigosdebarra = []
 
 def texto():
     os.system('cls')
     print('#########################################')
-    print('############ Menu Feria ##############')
+    print('############ Menu Produtos ##############')
     print('#########################################')
-    print('\n \t1 - Cadastrar Feira')
-    print('\n \t2 - Editar Feira')
-    print('\n \t3 - Remover Feira ou \n\tItens da Ultima feira')
-    print("\n \t4 - Listar Feira")
+    print('\n \t1 - Cadastrar Produtos')
+    print('\n \t2 - Editar Produtos')
+    print('\n \t3 - Remover Produtos')
+    print("\n \t4 - Listar Produtos")
     print("\n \t0 - Sair")
     print('#########################################')
     opcao = input()
@@ -50,27 +50,60 @@ def menu_feira():
     return
 
 def cadastrar_feira():
-    mais_um = True
-    print('Comece cadatrando os itens por categoria')
+    os.system('cls')
+    print('#########################################')
+    print('#######   Cadastras Produtos  ###########')
+    print('#########################################')
+    print('\n Comece cadastrando os itens por categoria')
     produtos.listar_categorias()
     vf = input('A categoria quer voce deseja cadastrar está na lista?\n\t Sim ou Nao')
     if vf !='Sim' and vf !='sim':
         produtos.cadastrar_categoria()
         produtos.listar_categorias()
-    
-    chavef = int(input('Deseja Cadatrar itens em qual categoria?')) -1
-    while mais_um != False:
-        item = input('informe o nome do produto')
-        itens.append(item)
-        itens_p.append(item)
-        preco = input('informe o preço do produo')
+    p=int(input('\n\t Informe quantos produtos deseja cadastrar \t'))
+    for i in range(p):
+        codigob = input('informe o codigo de barra do produto\t')
+        nome = input('Informe o nome do produto {}\t'.format(1+i))
+        itens.append(nome)
+        marca = input('Informe a marca do produto f\t')
+        itens.append(marca)
+        cat = input('Informe a categoria do produto f\t')
+        itens.append(cat)
+        uni = input('Informe a porção do produto (em kg) \n se o produto não for de alimentação pode reponder com a categoria\t')
+        itens.append(uni)
+        min = input('Informe a quantidade minima que voce precisa desse produto no mês\t')
+        itens.append(min)
+        atua = input('Informe a quantitade que voce comprou desse produto\t')
+        itens.append(atua)
+        preco = input('Informe o preço\t')
+        itens.append(preco)
         precos.append(preco)
-        feira.update({chavef:itens})
-        feira.update({'Preços':precos})
-        feira.update({'Itens':itens_p})
-        sair = input('Deseja cadastrar mais um produto?')
-        if sair !='sim':
-            mais_um = False
+        data = input('Informe a validade do produto\t')
+        itens.append(data)
+        feira.update({codigob:itens})
+
     return
 
-print(feira)
+
+ # mais_um = True
+    # print('Comece cadatrando os itens por categoria')
+    # produtos.listar_categorias()
+    # vf = input('A categoria quer voce deseja cadastrar está na lista?\n\t Sim ou Nao')
+    # if vf !='Sim' and vf !='sim':
+    #     produtos.cadastrar_categoria()
+    #     produtos.listar_categorias()
+    
+    # chavef = int(input('Deseja Cadatrar itens em qual categoria?')) -1
+    # while mais_um != False:
+    #     item = input('informe o nome do produto')
+    #     itens.append(item)
+    #     itens_p.append(item)
+    #     preco = input('informe o preço do produo')
+    #     precos.append(preco)
+    #     feira.update({chavef:itens})
+    #     feira.update({'Preços':precos})
+    #     feira.update({'Itens':itens_p})
+    #     sair = input('Deseja cadastrar mais um produto?')
+    #     if sair !='sim':
+    #         mais_um = False
+    # return
