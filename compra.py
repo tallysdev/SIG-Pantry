38d05a0886e5e0c11ae5b4a80ba1e9743975c20f
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-10 15:58:33 -0300

    tllys: teste salvado compras na data certa

--- compra.py
+++ compra.py
@@ -1,6 +1,38 @@
+import feira
 
-compr = {}
+# compras_do_dia_x
+cx = {}
+# codigosbarras_dos_produtos
+cp =[]
+z = []
+# for i in feira.feira.keys():
+#   cp.append(i)  
+# print(cp)
+
+for j in feira.datasdecompras:
+    for i in feira.feira.keys():
+        if feira.feira[i][8] == j:
+            cp.append(i)
+            cx.update({j:[cp,'somapreco']})
+
+
+# cx.update({j:[cp,'somapreco']})
+print(cx)
 
 def menu_compra():
     print()
     return
+
+def texto():
+    
+    print('#########################################')
+    print('############ Menu Compras ##############')
+    print('#########################################')
+    print('\n \t1 - Pesquidar nas Compras')
+    print('\n \t2 - Editar Compra')
+    print('\n \t3 - Remover Remover Compra')
+    print('\n \t4 - Acessar Produtos')
+    print("\n \t0 - Sair")
+    print('#########################################')
+    opcao = input()
+    return opcao
