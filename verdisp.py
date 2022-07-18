9575b0a4d8bfd7dd93112ce16f82a6aa2800b89a
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-18 12:06:57 -0300

    tallys:add menu de como vai ver a dispensa

--- verdisp.py
+++ verdisp.py
@@ -7,20 +7,39 @@ def textovd():
     print('\t 2 - Ver por data de validade \n\t "da mais recente a mais antiga" ')
     print('\t 3 - Ver por preço \n\t "do mais barato ao mais caro" ')
     print('\t 4 - Ver por categoria ')
-    opcao = input('\t')
+    try:
+        opcao = int(input())
+    except:
+        print("Opção inválida! Tente novamente.")
+        opcao = int(input())
     return opcao
 
 def menu_vd():
     opcao = textovd
-    while opcao != '0':
+    while opcao != 0:
         
-        if opcao == '1':
+        if opcao == 1:
             print()
-        elif opcao == '2':
+        elif opcao == 2:
             print()
-        elif opcao == '3':
+        elif opcao == 3:
             print()
-        elif opcao == '4':
+        elif opcao == 4:
             print()
-        
+    return
+
+def alfabeticav():
+    print()
+    return
+
+def validadev():
+    print()
+    return
+
+def precov():
+    print()
+    return
+
+def categoriav():
+    print()
     return
