1999e7f39f448c65e80d42521c005bdb71ef0e24
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-18 12:02:28 -0300

    tallys:add menu de como vai ver a dispensa

--- verdisp.py
+++ verdisp.py
@@ -1,3 +1,26 @@
-def menu_vd():
+def textovd():
+    print('#########################################')
+    print('########## Veja sua Dispensa ############')
+    print('#########################################')
     print()
+    print('\t 1 - Ver por ordem alfabetica ')
+    print('\t 2 - Ver por data de validade \n\t "da mais recente a mais antiga" ')
+    print('\t 3 - Ver por preço \n\t "do mais barato ao mais caro" ')
+    print('\t 4 - Ver por categoria ')
+    opcao = input('\t')
+    return opcao
+
+def menu_vd():
+    opcao = textovd
+    while opcao != '0':
+        
+        if opcao == '1':
+            print()
+        elif opcao == '2':
+            print()
+        elif opcao == '3':
+            print()
+        elif opcao == '4':
+            print()
+        
     return
