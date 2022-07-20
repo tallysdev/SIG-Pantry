0bc7e8b87b7e918f258fab1c34b028361763874a
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-19 21:04:53 -0300

    tallys: func texto de listagem

--- liscom.py
+++ liscom.py
@@ -1,3 +1,24 @@
+import os
+import feira
+
+
+def textoliscom():
+    os.system('cls')
+    print('#########################################')
+    print('########## Lista de Compras ############')
+    print('#########################################')
+    print('\n\t 1 - Lista de Compras com base em nos alimentos faltando ')
+    print('\n\t 2 - Quantas  ')
+    print('\n\t 3 - Ver por preço \n\t "do mais barato ao mais caro" ')
+    print('\n\t 4 - Ver por categoria ')
+    print('\n\t 0 - Sair ')
+    try:
+        opcao = int(input())
+    except:
+        print("Opção inválida! Tente novamente.")
+        opcao = int(input())
+    return opcao
+
 def menu_lc():
     print()
     return
