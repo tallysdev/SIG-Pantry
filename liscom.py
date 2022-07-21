137ae6149087d4d813d64b5d85c4c85bdfe96ce6
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-07-21 10:33:16 -0300

    tallys: primeiro esqueleto do terceiro modulo

--- liscom.py
+++ liscom.py
@@ -8,9 +8,10 @@ def textoliscom():
     print('########## Lista de Compras ############')
     print('#########################################')
     print('\n\t 1 - Lista de Compras com base em nos alimentos faltando ')
-    print('\n\t 2 - Quantas  ')
-    print('\n\t 3 - Ver por preço \n\t "do mais barato ao mais caro" ')
-    print('\n\t 4 - Ver por categoria ')
+    print('\n\t 2 - Lista de Compras com base na data de Validade  ')
+    print('\n\t 3 - Lista de Compras por categorias ')
+    print('\n\t 4 - Lista de Compras Geral ')
+    print('\n\t 5 - informações sobre este modulo ')
     print('\n\t 0 - Sair ')
     try:
         opcao = int(input())
@@ -20,5 +21,19 @@ def textoliscom():
     return opcao
 
 def menu_lc():
-    print()
+    opcao = textoliscom()
+
+    while opcao !=0:
+
+        if opcao == 1:
+            print()
+        elif opcao == 2:
+            print()
+        elif opcao == 3:
+            print()
+        elif opcao == 4:
+            print()
+        elif opcao == 5:
+            print()
     return
+
